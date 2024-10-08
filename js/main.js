60cf24f5311b9873a29a51ead3766127c2ecbf82
Dominic Jia Yi Lee <126468109+dlee0091@users.noreply.github.com>
2024-10-09 05:16:34 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var vg_1 = "js/hw9.vg.json"; 
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_2 = "js/hw10.vg.json";  
+vegaEmbed("#bubble_plot", vg_2).then(function(result) {
+    // Access the Vega view instance for the bubble plot
+}).catch(console.error);
+
+// var vg_3 = "js/not_grouped_bar_chart.vg.json";  
+// vegaEmbed("#dea", vg_3).then(function(result) {
+//     // Access the Vega view instance for the grouped bar chart
+// }).catch(console.error);
+
+var vg_4 = "js/radial.vg.json";  
+vegaEmbed("#radial_chart", vg_4).then(function(result) {
+    // Access the Vega view instance for the grouped bar chart
+}).catch(console.error);
+
+var vg_5 = "js/grouped_bar_chart.vg.json";  
+vegaEmbed("#grouped_bar_chart", vg_5).then(function(result) {
+    // Access the Vega view instance for the grouped bar chart
+}).catch(console.error);
+
+
+var vg_6 = "js/bubble_chart.vg.json";  
+vegaEmbed("#bubble_chart", vg_6).then(function(result) {
+    // Access the Vega view instance for the grouped bar chart
+}).catch(console.error);
+
